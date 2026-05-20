137cdb51cbcdc99fec19dd8247014adaffd5b0cc
yunji1014 <7787yunji@gmail.com>
2026-05-20 15:23:00 +0900

    setting

---
+++ src/main/java/com/codit/backend_a/BackendAApplication.java
@@ -0,0 +1,13 @@
+package com.codit.backend_a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendAApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codit/backend_a/BackendAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codit.backend_a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
